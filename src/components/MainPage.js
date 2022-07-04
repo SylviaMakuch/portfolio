ba4c03a12ed8f0285f84f9b4ceec3b79e6392191
Sylvia Makuch <sylvia@trimboli.io>
2022-07-04 16:25:58 -0600

    added gradient

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -4,20 +4,28 @@ import Navbar from "./Navbar.js";
 import Skills from "./Skills.js";
 import Projects from "./Projects/Projects.js";
 import Hero from "./Hero.js";
-import Principles from "./Principles/Principles"
+import AboutMe from "./AboutMe/AboutMe"
 import ContactForm from "./ContactForm.js";
 
-const MainContainer = styled.div``;
+const GradientMapBackground = styled.div`
+  background-color: #000000;
+  background-image: 
+  radial-gradient(at 33% 27%,hsl(290deg 69% 28% / 62%) 0,transparent 60%), 
+  radial-gradient(at 74% 70%,hsl(260deg 55% 36%) 0,transparent 80%);
+`;
+
 
 export default function MainPage() {
   return (
-    <MainContainer>
+    <>
       <Navbar />
       <Hero />
       <Skills />
-      <Principles />
-      <Projects />
+      <GradientMapBackground>
+        <AboutMe />
+        <Projects />
+      </GradientMapBackground>
       <ContactForm />
-    </MainContainer>
+    </>
   );
 }
