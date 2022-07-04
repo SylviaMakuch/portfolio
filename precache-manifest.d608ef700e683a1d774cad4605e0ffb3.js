eac93bbeb7d1eda8e38b78ae384d796a9dc814b6
Sylvia Makuch <sylvia@trimboli.io>
2022-07-03 23:00:56 -0600

    Updates

---
+++ precache-manifest.d608ef700e683a1d774cad4605e0ffb3.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = [
+  {
+    "revision": "1304950c71b6cd9f667e",
+    "url": "/portfolio/static/css/main.b66a17b9.chunk.css"
+  },
+  {
+    "revision": "1304950c71b6cd9f667e",
+    "url": "/portfolio/static/js/main.1304950c.chunk.js"
+  },
+  {
+    "revision": "763f0ab924c8974bad5b",
+    "url": "/portfolio/static/css/1.e690bace.chunk.css"
+  },
+  {
+    "revision": "763f0ab924c8974bad5b",
+    "url": "/portfolio/static/js/1.763f0ab9.chunk.js"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "f517c25002754ffc6deb4ad49a3f2300",
+    "url": "/portfolio/static/media/github.f517c250.svg"
+  },
+  {
+    "revision": "03c84bcc3edf09694692ee18fa86d163",
+    "url": "/portfolio/static/media/bootstrap.03c84bcc.svg"
+  },
+  {
+    "revision": "2db97d69ba44f8da4663584e8197143f",
+    "url": "/portfolio/static/media/css.2db97d69.svg"
+  },
+  {
+    "revision": "092309206a296aefb302ffe6ba5484c1",
+    "url": "/portfolio/static/media/figma.09230920.svg"
+  },
+  {
+    "revision": "4b7156791ee09be5d834d28fe017ec10",
+    "url": "/portfolio/static/media/html.4b715679.svg"
+  },
+  {
+    "revision": "50effcf9a2e8ec723cbfc6dc1bce9405",
+    "url": "/portfolio/static/media/js.50effcf9.svg"
+  },
+  {
+    "revision": "409452f1b3c56ada4efdcfbd39ff1142",
+    "url": "/portfolio/static/media/mongodb.409452f1.svg"
+  },
+  {
+    "revision": "c10c6d6d8ee3fd097f627869a1757658",
+    "url": "/portfolio/static/media/react.c10c6d6d.svg"
+  },
+  {
+    "revision": "d45883987aa53c3ea22e33b7a0f6e4da",
+    "url": "/portfolio/static/media/ts.d4588398.svg"
+  },
+  {
+    "revision": "d3af788e91699eac478e9e31d8cefc31",
+    "url": "/portfolio/static/media/threejslogo.d3af788e.svg"
+  },
+  {
+    "revision": "e670a3813ecbc475743770114690d581",
+    "url": "/portfolio/static/media/ownme.e670a381.png"
+  },
+  {
+    "revision": "155f344516f88ddbec87b90bed6dea3d",
+    "url": "/portfolio/static/media/randomqoute.155f3445.png"
+  },
+  {
+    "revision": "b17185aad924febb12f7a7558494418e",
+    "url": "/portfolio/static/media/soundboard.b17185aa.png"
+  },
+  {
+    "revision": "bf59072d91f1bd04a9b6e597fb9e6fa0",
+    "url": "/portfolio/static/media/tinpurr.bf59072d.gif"
+  },
+  {
+    "revision": "5daa412379a9a651894f299741ea2e0d",
+    "url": "/portfolio/static/media/artpage.5daa4123.svg"
+  },
+  {
+    "revision": "82bcfcbe485152c44489e5cb138508dd",
+    "url": "/portfolio/static/media/threejsprofolio.82bcfcbe.gif"
+  },
+  {
+    "revision": "fdaf47f3bada3f179e3b3cddcb213f10",
+    "url": "/portfolio/static/media/thecookbook.fdaf47f3.png"
+  },
+  {
+    "revision": "1a54295438e0f9e1ec0bca1aab3846c8",
+    "url": "/portfolio/static/media/feellings.1a542954.png"
+  },
+  {
+    "revision": "29e7079f812d2f2b54cb157b6e37b6b9",
+    "url": "/portfolio/static/media/workhard.29e7079f.gif"
+  },
+  {
+    "revision": "38809fa09125c8aa0848edbab8456fb9",
+    "url": "/portfolio/static/media/wavey.38809fa0.gif"
+  },
+  {
+    "revision": "38ed9321b03324ae3c4563160eefd720",
+    "url": "/portfolio/static/media/profilepic.38ed9321.jpg"
+  },
+  {
+    "revision": "5a41ea9f83061007949ada7a82fcce4f",
+    "url": "/portfolio/static/media/linkden.5a41ea9f.svg"
+  },
+  {
+    "revision": "a2e7203fdb08400543184dd4fa98f6a7",
+    "url": "/portfolio/static/media/eng.a2e7203f.png"
+  },
+  {
+    "revision": "b2afcffca91bf05f2b17e4adc7bc8021",
+    "url": "/portfolio/static/media/design.b2afcffc.png"
+  },
+  {
+    "revision": "db9a77e1e0cecbd39043a89ca740ba79",
+    "url": "/portfolio/static/media/webdev.db9a77e1.png"
+  },
+  {
+    "revision": "0db92e90a3f558e51654927483d4b7b4",
+    "url": "/portfolio/static/media/animatedwave.0db92e90.svg"
+  },
+  {
+    "revision": "53068882d08652055fb2861975a3aa08",
+    "url": "/portfolio/static/media/resume.53068882.svg"
+  },
+  {
+    "revision": "f004f465fe944426054126949fad65f4",
+    "url": "/portfolio/index.html"
+  }
+];
